22f78519917738597a524e116cae90403518fc04
Martins <martins.zeltins@arkbauer.com>
2022-10-02 14:17:57 +0300

    Cleanup

--- src/window.c
+++ src/window.c
@@ -3,25 +3,23 @@
 
 struct _ShowhideWindow
 {
-  AdwApplicationWindow  parent_instance;
-
-  /* Template widgets */
-  GtkHeaderBar        *header_bar;
-  GtkLabel            *label;
+    AdwApplicationWindow  parent_instance;
+    GtkHeaderBar         * header_bar;
+    GtkLabel             * label;
 };
 
 G_DEFINE_FINAL_TYPE (ShowhideWindow, showhide_window, ADW_TYPE_APPLICATION_WINDOW)
 
-static void showhide_window_class_init (ShowhideWindowClass *klass)
+static void showhide_window_class_init (ShowhideWindowClass * class)
 {
-  GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+    GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (class);
 
-  gtk_widget_class_set_template_from_resource (widget_class, "/org/martins/ShowHide/window.ui");
-  gtk_widget_class_bind_template_child (widget_class, ShowhideWindow, header_bar);
-  gtk_widget_class_bind_template_child (widget_class, ShowhideWindow, label);
+    gtk_widget_class_set_template_from_resource (widget_class, "/org/martins/ShowHide/window.ui");
+    gtk_widget_class_bind_template_child (widget_class, ShowhideWindow, header_bar);
+    gtk_widget_class_bind_template_child (widget_class, ShowhideWindow, label);
 }
 
-static void showhide_window_init (ShowhideWindow *self)
+static void showhide_window_init (ShowhideWindow * self)
 {
-  gtk_widget_init_template (GTK_WIDGET (self));
+    gtk_widget_init_template (GTK_WIDGET (self));
 }
