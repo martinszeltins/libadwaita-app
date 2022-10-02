8c50cea616942783d57f860042008756f11a9dc0
Martins <martins.zeltins@arkbauer.com>
2022-10-02 14:19:25 +0300

    Cleanup

--- src/window.c
+++ src/window.c
@@ -1,25 +1,25 @@
 #include "config.h"
 #include "window.h"
 
-struct _ShowhideWindow
+struct _MainWindow
 {
     AdwApplicationWindow  parent_instance;
     GtkHeaderBar         * header_bar;
     GtkLabel             * label;
 };
 
-G_DEFINE_FINAL_TYPE (ShowhideWindow, showhide_window, ADW_TYPE_APPLICATION_WINDOW)
+G_DEFINE_FINAL_TYPE (MainWindow, showhide_window, ADW_TYPE_APPLICATION_WINDOW)
 
-static void showhide_window_class_init (ShowhideWindowClass * class)
+static void showhide_window_class_init (MainWindowClass * class)
 {
     GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (class);
 
     gtk_widget_class_set_template_from_resource (widget_class, "/org/martins/ShowHide/window.ui");
-    gtk_widget_class_bind_template_child (widget_class, ShowhideWindow, header_bar);
-    gtk_widget_class_bind_template_child (widget_class, ShowhideWindow, label);
+    gtk_widget_class_bind_template_child (widget_class, MainWindow, header_bar);
+    gtk_widget_class_bind_template_child (widget_class, MainWindow, label);
 }
 
-static void showhide_window_init (ShowhideWindow * self)
+static void showhide_window_init (MainWindow * self)
 {
     gtk_widget_init_template (GTK_WIDGET (self));
 }
--- src/window.h
+++ src/window.h
@@ -6,6 +6,6 @@ G_BEGIN_DECLS
 
 #define TYPE_WINDOW (showhide_window_get_type())
 
-G_DECLARE_FINAL_TYPE (ShowhideWindow, showhide_window, SHOWHIDE, WINDOW, AdwApplicationWindow)
+G_DECLARE_FINAL_TYPE (MainWindow, showhide_window, SHOWHIDE, WINDOW, AdwApplicationWindow)
 
 G_END_DECLS
