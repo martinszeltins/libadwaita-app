2551f42ed829c8661fc9b12cea5d3689883e4aba
Martins <martins.zeltins@arkbauer.com>
2022-10-02 14:20:35 +0300

    Cleanup

--- src/window.c
+++ src/window.c
@@ -12,14 +12,14 @@ G_DEFINE_FINAL_TYPE (MainWindow, showhide_window, ADW_TYPE_APPLICATION_WINDOW)
 
 static void showhide_window_class_init (MainWindowClass * class)
 {
-    GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (class);
+    GtkWidgetClass * widget_class = GTK_WIDGET_CLASS (class);
 
     gtk_widget_class_set_template_from_resource (widget_class, "/org/martins/ShowHide/window.ui");
     gtk_widget_class_bind_template_child (widget_class, MainWindow, header_bar);
     gtk_widget_class_bind_template_child (widget_class, MainWindow, label);
 }
 
-static void showhide_window_init (MainWindow * self)
+static void showhide_window_init (MainWindow * window)
 {
-    gtk_widget_init_template (GTK_WIDGET (self));
+    gtk_widget_init_template (GTK_WIDGET (window));
 }
