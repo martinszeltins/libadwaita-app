3f5c3e50e3963e784d845c18ce7168d7795efc4d
Martins <martins.zeltins@arkbauer.com>
2022-10-02 13:26:29 +0300

    Cleanup

--- src/main.c
+++ src/main.c
@@ -5,14 +5,12 @@
 int main (int argc, char *argv[])
 {
   g_autoptr(ShowhideApplication) app = NULL;
-  int ret;
 
   bindtextdomain (GETTEXT_PACKAGE, LOCALEDIR);
   bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
   textdomain (GETTEXT_PACKAGE);
 
   app = showhide_application_new ("org.martins.ShowHide", G_APPLICATION_FLAGS_NONE);
-  ret = g_application_run (G_APPLICATION (app), argc, argv);
-
-  return ret;
+  
+  return g_application_run (G_APPLICATION (app), argc, argv);
 }
